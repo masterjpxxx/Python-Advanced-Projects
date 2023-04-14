82a980905db958fc6ccd55358994bf212697a603
masterjpxxx <johnpearl.manungas@pantheon.io>
2023-04-14 23:04:13 +0800

    Regex Phone number checker

---
+++ Automate-the-Boring-Stuff-with-Python/checkphonenumber.py
@@ -0,0 +1,40 @@
+import re
+
+#Format should be 415-555-4242
+
+def checkphonenumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    
+    if text[3] != '-':
+        return False
+    
+    for i in range (4, 7):
+        if not text[i].isdecimal():
+            return False
+    
+    if text[7] != '-':
+        return  False
+    
+    for i in range (8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+#Regex Method
+
+def isphoneregex(text):
+   regex =  re.compile(r'(\d{3}-)?(\d{3})-(\d{4})')
+   search = regex.search(text)
+   return search.group()
+
+
+while(True):
+    phone = input("Input message: ")
+    print(f'The phone number is {isphoneregex(phone)}')
+    #print(checkphonenumber(phone))
+    
+    
